05a4f77b170979f2fcf5cd5b9ef447f618855fb3
argentum47 <princeofpersiaa3.ag@gmail.com>
2015-05-14 23:48:15 +0530

    Step 3: adding angular specific manifest files for javascripts and stylesheets

---
+++ app/assets/javascripts/ngapplication.js
@@ -0,0 +1,13 @@
+//= require angular
+//= require angular-route
+//= require angular-resource
+//= require lodash
+//= require restangular
+//= require angular-rails-templates
+
+//= require magazine_app
+//= require_tree ../../../ngapp/controllers
+//= require_tree ../../../ngapp/services
+//= require_tree ../../../ngapp/filters
+//= require_tree ../../../ngapp/directives
+//= require_tree ../../../ngapp/templates
